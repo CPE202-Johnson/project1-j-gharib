c73f102719dd06c4c470ecdbd6b815baeb4abf3d
j-gharib <jgharib@calpoly.edu>
2021-01-17 17:18:55 -0800

    trial push 2

--- base_convert_testcases.py
+++ base_convert_testcases.py
@@ -12,6 +12,14 @@ class TestBaseConvert(unittest.TestCase):
     def test_base16(self):
         self.assertEqual(convert(316,16),"13C")
     
+    def test_base16(self):
+        self.assertEqual(convert(10,16),"A")
+        self.assertEqual(convert(11,16),"B")
+        self.assertEqual(convert(12,16),"C")
+        self.assertEqual(convert(13,16),"D")
+        self.assertEqual(convert(14,16),"E")
+        self.assertEqual(convert(15,16),"F")
+
     def test_baseError(self):
         with self.assertRaises(ValueError):  # used to check for exception
             convert(None, 1)
